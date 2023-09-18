23efb42d164bc0ac9a9a3d97a2d053483daf93c7
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-18 17:43:16 +0600

    Update if_else.php

--- if_else.php
+++ if_else.php
@@ -9,3 +9,17 @@ if ($remainder == 0){
     
     echo"The number $number is odd.";
 }
+
+<?php
+
+$number = 30;
+$remainder = $number % 2;
+if ($number == 0){
+    echo "The number $number is zero.";
+}else if ($remainder == 0){
+
+    echo"The number $number is even.";
+}else{
+
+    echo"The number $number is odd.";
+}
