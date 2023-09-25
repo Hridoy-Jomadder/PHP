0cdb8f52f60962a6bbf9de585715cbf33ea7ca18
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-25 23:56:52 +0600

    Update Task 03.php

--- Task 03.php
+++ Task 03.php
@@ -47,4 +47,56 @@ Question :
 
 
 Answer:
- 
+ Task 1:
+<?php
+//Associative Array
+$student=['name'=>"Alice","age"=>"22","grade"=>"A"];
+
+//ForEach Loop Through an Associative Array
+foreach($student as $key=>$value){
+
+    echo "$key: $value";
+}
+?>
+
+Task 2:
+<?php
+$student=['name'=>"Alice","age"=>"22","grade"=>"A"];
+$check=array_key_exists('grade',$student);
+echo($check);
+
+Task 3:
+<?php
+
+$numbers = [100, 200, 50, 40, 50];
+foreach($numbers as $number){
+echo $number . "";
+}
+
+
+Task 4:
+<?php
+// Create the array
+$names = ['Talha', 'Afnan', 'Mashrufa', 'Zia', 'Iqbal', 'Habib', 'Airin', 'Moni'];
+
+// Use array_filter() to filter names starting with 'M'
+$filteredNames = array_filter($names, function($name) {
+    return strpos($name, 'M') === 0;
+});
+
+// Print the filtered names
+foreach ($filteredNames as $name) {
+    echo $name . "\n";
+}
+?>
+
+Task 5:
+<?php
+$originalString = 'Hello, World!';
+
+// Use strrev() to reverse the string
+$reversedString = strrev($originalString);
+
+// Print the reversed string
+echo $reversedString;
+?>
