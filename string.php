a6a35e12cd16cb50810a5b4db7bc64300c651d67
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-26 21:16:04 +0600

    Update string.php

--- string.php
+++ string.php
@@ -4,3 +4,11 @@ $firstname = "Hridoy";
 $lastname = "Jomadder";
 $fullname = $firstname ." ". $lastname;
 echo $fullname;
+
+//Array to string conversion
+$fruits = ["Apple","Banana","Orange"];
+echo $fruits[1];
+
+//Array and String Manipulation with Utility Functions
+$string = " Hello World";
+echo $string[1];
