13541d7d149d707fb1dc658f2675d31689eb00f2
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-09-27 19:22:09 +0600

    Update string.php

--- string.php
+++ string.php
@@ -19,9 +19,16 @@ $lenght = strlen($string);
 for($i=0;$i<$lenght;$i++){
     echo strlen($i);
 }
+//3
+$string = " Hello World";
+echo substr($string,0,5); //hello
 
+//4
+$string = " Hello World";
+$lenght = strlen($string);
+$startingPosition = $lenght - 5;
 
-
+ echo substr($string,$startingPosition); 
 
 
 
