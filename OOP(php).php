58bb855f48c280f75e2dc4848edc9902be062621
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-10-02 21:02:20 +0600

    Update OOP(php).php

--- OOP(php).php
+++ OOP(php).php
@@ -1,5 +1,17 @@
 <?php
-1. Class a allow developers to encapsulate related data and function into a single entity.
+
+/*
+Class & Object
+1. Class a allow developers to encapsulate related data and functions into a single entity.
 2. Making it easier to manage and extend code.
-3. An Object is an 
-class
+3. An Object is an instance of a class. You create an object of a class using the new keyword.
+*/
+class OSTAD{
+    public $name="PHP BATCH 02";
+    public function demo(){
+    echo $this->name;
+    }
+
+ }
+ $obj=new OSTAD();
+$obj->demo();
