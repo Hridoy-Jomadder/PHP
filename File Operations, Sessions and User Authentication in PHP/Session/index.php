834c71d177ec7142e3221266fa2c0780c513d161
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-10-29 21:29:48 +0600

    Update index.php

--- File Operations, Sessions and User Authentication in PHP/Session/index.php
+++ File Operations, Sessions and User Authentication in PHP/Session/index.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['username'])){
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Animated Login & Registration Form | Code</title>
+  <title>Animated Login & Registration Form </title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
