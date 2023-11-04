992df47f6c8e20b8ed9edf0075fbf5ec4f8e89e3
Hridoy Jomadder <79590693+Hridoy-Jomadder@users.noreply.github.com>
2023-11-04 23:16:21 +0600

    Update Task 06.php

--- Task 06.php
+++ Task 06.php
@@ -9,3 +9,28 @@ Write a SQL query to retrieve all the orders from the 'orders' table and their c
 Task 2: .
 
 Write a SQL query to retrieve all the products from the 'products' table and their corresponding category names from the 'categories' table. Use a LEFT JOIN to combine the tables and include all products, even if they don't have a category.
+
+Answer:
+Task 1:
+/* For the 'orders' table:
+ Primary key: order_id
+Foreign key (referencing 'customer_id' from 'customers' table)
+
+For the 'customers' table:
+Primary key: customer_id */
+
+SELECT a.order_id, c.customer_name
+FROM orders a
+INNER JOIN customers c ON a.customer_id = c.customer_id;
+
+Task 2: 
+/* For the 'products' table:
+Primary key: product_id
+Foreign key (referencing 'category_id' from 'categories' table)
+
+For the 'categories' table:
+Primary key: category_id */
+
+SELECT a.product_id, a.product_name, c.category_name
+FROM products a
+LEFT JOIN categories c ON a.category_id = c.category_id;
